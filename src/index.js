206994fcc592caee5c655b65c5d808a2b4cd5fd4
jukebox101 <leonora.squires@gmail.com>
2020-06-24 00:25:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   // your code here
-});
+  const form = document.querySelector('#create-task-form')
+
+  form.addEventListener("submit", function (event) {
+
+    event.preventDefault()
+
+
+  const task = event.target.newTaskDescription.value
+
+  const listingObj = {
+    newTaskDescription: task
+  }
+
+  console.log(listingObj)
+
+  renderOneListing(listingObj)
+  })
+
+  const taskSection = document.querySelector("#tasks")
+
+  function renderOneListing(listingObj){
+
+    const taskList = document.createElement("ul")
+    taskList.classList.add("task")
+
+    taskList.innerHTML =
+     `  
+      <li>${listingObj.newTaskDescription}</li>
+
+      `
+      taskSection.append(taskList)
+  }
+
+})
